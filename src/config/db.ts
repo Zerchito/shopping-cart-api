95544cc6ef1a7b9047cce226a51bee69b94355b8
zerch <zerchito@gmail.com>
2025-12-19 20:00:55 -0400

    chore: configure database connection

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,7 +2,6 @@ import mongoose from 'mongoose';
 
 export const connectDB = async (): Promise<void> => {
     const mongoUri = process.env.MONGO_URI;
-
     if (!mongoUri) {
         throw new Error('MONGO_URI is not defined');
     }
