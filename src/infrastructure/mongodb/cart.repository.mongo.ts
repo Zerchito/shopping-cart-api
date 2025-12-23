96839e7d0a54e40055bce56e4c1e6ceb5a875fc9
zerch <zerchito@gmail.com>
2025-12-23 00:04:26 -0400

    feat(cart): implement mongo cart repository

---
+++ src/infrastructure/mongodb/cart.repository.mongo.ts
@@ -0,0 +1,30 @@
+import { CartRepository } from '../../domain/cart/cart.repository';
+import { Cart } from '../../domain/cart/cart.types';
+import { CartModel } from './cart.model';
+
+export class MongoCartRepository implements CartRepository {
+  async save(cart: Cart): Promise<Cart> {
+    const created = await CartModel.create(cart);
+    return {
+      items: created.items,
+      totalItems: cart.totalItems
+    };
+  }
+
+  async findById(id: string): Promise<Cart | null> {
+    const found = await CartModel.findById(id).lean();
+    if (!found) return null;
+
+    return {
+      items: found.items,
+      totalItems: found.items.reduce(
+        (sum, item) => sum + item.quantity,
+        0
+      )
+    };
+  }
+
+  async deleteById(id: string): Promise<void> {
+    await CartModel.findByIdAndDelete(id);
+  }
+}
